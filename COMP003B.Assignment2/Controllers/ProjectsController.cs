6810ff57dbfc05a38fafe6b360d93eb7d5acedaf
gonzalez-7 <20agonzalezp@gmail.com>
2025-03-27 16:54:08 -0700

    Created a ProjectsController and Views for ProjectsController, modify the _Layout.cshtml for navigation

---
+++ COMP003B.Assignment2/Controllers/ProjectsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assignment2.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        public IActionResult Current()
+        {
+            return View();
+        }
+
+        public IActionResult Completed()
+        {
+            return View();
+        }
+
+        public IActionResult Ideas()
+        {
+            return View();
+        }
+    }
+}
--- COMP003B.Assignment2/Middleware/RequestTrackerMiddleware.cs
+++ COMP003B.Assignment2/Middleware/RequestTrackerMiddleware.cs
@@ -2,5 +2,20 @@
 {
     public class RequestTrackerMiddleware
     {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<RequestTrackerMiddleware> _logger;
+
+        public RequestTrackerMiddleware(RequestDelegate next, ILogger<RequestTrackerMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            _logger.LogInformation($"Request: {context.Request.Method} {context.Request.Path}");
+            await _next(context);
+            _logger.LogInformation($"Response Status: {context.Response.StatusCode}");
+        }
     }
 }
