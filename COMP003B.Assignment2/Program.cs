5b99f2253a4e022928ddd100a3a4551c919c43eb
gonzalez-7 <20agonzalezp@gmail.com>
2025-03-27 17:56:42 -0700

    fixed some problems

--- COMP003B.Assignment2/Program.cs
+++ COMP003B.Assignment2/Program.cs
@@ -5,6 +5,8 @@ Faculty: Jonathan Cruz
 Purpose: This application demonstrates MVC concepts, including middleware, controllers, views, and layout design.
 */
 
+using COMP003B.Assignment2.Middleware;  
+
 namespace COMP003B.Assignment2
 {
     public class Program
@@ -13,12 +15,12 @@ namespace COMP003B.Assignment2
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            // Add services to the container
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            // Configure the HTTP request pipeline
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
@@ -34,13 +36,10 @@ namespace COMP003B.Assignment2
             app.UseWelcomePage("/welcome");
 
             // Register custom middleware
-            app.UseMiddleware<RequestTrackerMiddleware>();
+            app.UseMiddleware<COMP003B.Assignment2.Middleware.RequestTrackerMiddleware>();
 
             app.UseRouting();
             app.UseAuthorization();
-
-
-            app.MapStaticAssets();
 
             // Default MVC routing
             app.MapControllerRoute(
